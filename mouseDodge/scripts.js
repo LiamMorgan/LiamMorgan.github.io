20e6ce7f537b4dc3f1b89e09c1133759cf856f37
Liam Morgan <liammorgan128@gmail.com>
2017-05-23 19:06:06 +0100

    Image?

--- mouseDodge/scripts.js
+++ mouseDodge/scripts.js
@@ -92,7 +92,7 @@ function drawEnd() {
 	text(rocks.length, width/2, height/2);
 	textSize(60);
 	text("Press space to restart.", width/2.7, height/2+100);
-	image(img, width/2-img.width, 20, 300, 300);
+	image(img, width/2-img.width, 20);
 }
 function keyPressed() {
 	if(keyCode == 32 && gameState == true) {
